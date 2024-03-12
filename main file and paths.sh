4043d1e737bda1b69a324dad0b632c92fa1adbd1
ZiyuMeng98 <154886834+ZiyuMeng98@users.noreply.github.com>
2024-03-12 11:23:35 +0800

    Create main file and paths.sh

---
+++ main file and paths.sh
@@ -0,0 +1,16 @@
+#Specify several key files and paths for GBrowse
+#Installation directory of GBrowse, where the bin directory contains many important programs, such as user programs for creating GBrowse
+/opt/biosoft/GBrowse-2.56/
+#Directory for storing GBrowse configuration files, including the global configuration file GBrowse.conf and a configuration file for a specific species created by oneself.
+/etc/gbrowse2/
+#The directory containing some web files of GBrowse2, such as usage tutorials, etc
+/var/www/html/gbrowse2/
+#Executable files of GBrowse2 web application
+/var/www/cgi-bin/gb2/
+#The database directory and user directory of GBrowse2,and so on.It is recommended to modify the owner of the database directory for importing data
+/var/lib/gbrowse2/
+#The temporary data storage path for GBrowse2
+/var/tmp/gbrowse2/
+#Configuration information of GBrowse in web service
+/etc/httpd/conf.d/z_gbrowse2.conf
+/etc/httpd/conf.d/gbrowse2.conf
